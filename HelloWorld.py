85318341ab3c79ed20973ef363a3082cd5ccd56d
noahm44 <60442041+noahm44@users.noreply.github.com>
2021-03-23 15:19:12 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      zoahm
+#
+# Created:     23/03/2021
+# Copyright:   (c) zoahm 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print ("Hello World, my name is Noah and I love Python!")
