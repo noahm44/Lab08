af3bb7e0ccc9ed45697ed59c6e44cc33336923ea
noahm44 <60442041+noahm44@users.noreply.github.com>
2021-03-23 15:32:59 -0400

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      zoahm
+#
+# Created:     23/03/2021
+# Copyright:   (c) zoahm 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+import hashlib
+print (hashlib.md5(b"hello").hexdigest())
