064f0844b02015620c447f7a260b7a3aae4dcd0f
noahm44 <60442041+noahm44@users.noreply.github.com>
2021-03-23 15:22:58 -0400

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      zoahm
+#
+# Created:     23/03/2021
+# Copyright:   (c) zoahm 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+userName = input ("Hello! Please enter you name!")
+
+print ("Hello", userName, "nice to meet you")
